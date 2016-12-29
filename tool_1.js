6804cec21712488e26cb4f9fdec13077a6cf2ce6
ted <linyc1983@gmail>
2016-12-29 19:08:54 +0800

    add singleton

--- tool_1.js
+++ tool_1.js
@@ -252,3 +252,69 @@ var size26 = makeSizer(26);
 document.getElementById('size-12').onclick = size12;
 document.getElementById('size-14').onclick = size14;
 document.getElementById('size-26').onclick = size26;
+
+
+getXById = function() {
+    var id = this.id;
+    $.ajax({
+        url:'/getx?id=' + id,
+        success: function(response) {
+            console.log(response);
+        }
+    });
+}
+$('.test').on('click', getXById);
+
+
+
+
+var SingletonTester = (function () {
+ 
+  // options: an object containing configuration options for the singleton
+  // e.g var options = { name: "test", pointX: 5};
+  function Singleton( options ) {
+ 
+    // set options to the options supplied
+    // or an empty object if none are provided
+    options = options || {};
+ 
+    // set some properties for our singleton
+    this.name = "SingletonTester111";
+ 
+    this.pointX = options.pointX || 6;
+ 
+    this.pointY = options.pointY || 10;
+ 
+  }
+ 
+  // our instance holder
+  var instance;
+ 
+  // an emulation of static variables and methods
+  var _static = {
+ 
+    name: "SingletonTester222",
+ 
+    // Method for getting an instance. It returns
+    // a singleton instance of a singleton object
+    getInstance: function( options ) {
+      if( instance === undefined ) {
+        instance = new Singleton( options );
+      }
+ 
+      return instance;
+ 
+    }
+  };
+ 
+  return _static;
+ 
+})();
+ 
+var singletonTest = SingletonTester.getInstance({
+  pointX: 5
+});
+ 
+// Log the output of pointX just to verify it is correct
+// Outputs: 5
+console.log( singletonTest.pointX );
