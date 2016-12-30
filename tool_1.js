fce689dc313c6023ee02859af31758462ad8850a
ted <linyc1983@gmail>
2016-12-30 09:52:03 +0800

    add observer

--- tool_1.js
+++ tool_1.js
@@ -318,3 +318,90 @@ var singletonTest = SingletonTester.getInstance({
 // Log the output of pointX just to verify it is correct
 // Outputs: 5
 console.log( singletonTest.pointX );
+
+
+
+
+function ObserverList(){
+  this.observerList = [];
+}
+ObserverList.prototype.add = function( obj ){
+  return this.observerList.push( obj );
+};
+ObserverList.prototype.count = function(){
+  return this.observerList.length;
+};
+ObserverList.prototype.get = function( index ){
+  if( index > -1 && index < this.observerList.length ){
+    return this.observerList[ index ];
+  }
+};
+ObserverList.prototype.indexOf = function( obj, startIndex ){
+  var i = startIndex;
+ 
+  while( i < this.observerList.length ){
+    if( this.observerList[i] === obj ){
+      return i;
+    }
+    i++;
+  }
+ 
+  return -1;
+};
+ObserverList.prototype.removeAt = function( index ){
+  this.observerList.splice( index, 1 );
+};
+<button id="addNewObserver">Add New Observer checkbox</button>
+<input id="mainCheckbox" type="checkbox"/>
+<div id="observersContainer"></div>
+// Extend an object with an extension
+function extend( obj, extension ){
+  for ( var key in extension ){
+    obj[key] = extension[key];
+  }
+}
+// References to our DOM elements
+var controlCheckbox = document.getElementById( "mainCheckbox" ),
+  addBtn = document.getElementById( "addNewObserver" ),
+  container = document.getElementById( "observersContainer" );
+// Concrete Subject
+// Extend the controlling checkbox with the Subject class
+extend( controlCheckbox, new Subject() );
+// Clicking the checkbox will trigger notifications to its observers
+controlCheckbox.onclick = function(){
+  controlCheckbox.notify( controlCheckbox.checked );
+};
+addBtn.onclick = addNewObserver;
+// Concrete Observer
+function addNewObserver(){
+  // Create a new checkbox to be added
+  var check = document.createElement( "input" );
+  check.type = "checkbox";
+  // Extend the checkbox with the Observer class
+  extend( check, new Observer() );
+  // Override with custom update behaviour
+  check.update = function( value ){
+    this.checked = value;
+  };
+  // Add the new observer to our list of observers
+  // for our main subject
+  controlCheckbox.addObserver( check );
+  // Append the item to the container
+  container.appendChild( check );
+}
+// The Observer
+function Observer(){
+  this.update = function(){
+    // ...
+  };
+}
+
+
+
+
+
+
+
+
+
+
